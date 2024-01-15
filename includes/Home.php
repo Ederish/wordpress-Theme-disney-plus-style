54f417b0d010cd74be770e4e02420e4025d46038
Ederish <63983547+Ederish@users.noreply.github.com>
2024-01-15 17:42:40 -0600

    Delete includes directory

--- includes/Home.php
+++
@@ -1,9 +0,0 @@
-<main class="home">
-      <div class="home__container movieRows__container">
-  <?php
-       get_template_part('includes\ImageSlider');
-       get_template_part('includes\Collections');
-       get_template_part('includes\MoviesRows');
-     ?>
-      </div>
-    </main>  
